b1ea178aa30e70f41850dbf9045319a6617c48da
eprikhodko <eprikhodkodev@gmail.com>
2023-11-17 14:28:03 +0100

    setuo initial vite development server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
+  root: resolve(__dirname, "src"),
   plugins: [
     handlebars({
       context: {
