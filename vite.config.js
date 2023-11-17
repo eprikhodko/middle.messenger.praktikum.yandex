312b4b5f9ffb3dc82eec1b85e0c2fa0b1bc650f3
eprikhodko <eprikhodkodev@gmail.com>
2023-11-17 17:55:35 +0100

    add multi page build support

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
   root: resolve(__dirname, "src"),
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        signIn: resolve(__dirname, "src/sign-in.html"),
+      },
+    },
+  },
   plugins: [
     handlebars({
       context: {
