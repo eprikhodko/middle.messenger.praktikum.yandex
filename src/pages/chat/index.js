6847085a74fe3c625dd6428237ebdc46f8a3a9f5
eprikhodko <eprikhodkodev@gmail.com>
2023-11-22 17:10:46 +0100

    create basic chat structure

---
+++ src/pages/chat/index.js
@@ -0,0 +1,11 @@
+// global style
+import "@src/main.css";
+// page style
+import "./chat.css";
+// page and layout components
+import "@components/layout/default";
+import "@components/layout/formCommonLayout";
+import "@components/FormSignUp";
+import "@components/ButtonCommon";
+import "@components/LinkCommon";
+import "@components/FormInput";
