7e94ff0ad8ae4ee90531fe8b67ae230eac9698d5
eprikhodko <eprikhodkodev@gmail.com>
2023-11-26 21:40:28 +0100

    create initial profile-page layout

---
+++ src/pages/profile/index.js
@@ -0,0 +1,12 @@
+// global style
+import "@src/main.css";
+// page style
+import "./profile.css";
+// layouts, components, modules
+import "@components/layout/default";
+import "@components/layout/formCommonLayout";
+import "@modules/Chat";
+import "@modules/Chat/components/SearchInput";
+import "@modules/Chat/components/LinkProfile";
+import "@components/ButtonArrow"
+
