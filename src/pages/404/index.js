64f50c50ac9befbc7969a6c7dfe683a9f72427ca
eprikhodko <eprikhodkodev@gmail.com>
2023-11-26 23:51:53 +0100

    add error pages

---
+++ src/pages/404/index.js
@@ -0,0 +1,12 @@
+// global style
+import "@src/main.css";
+// page style
+import "./404.css";
+// layouts, components, modules
+import "@components/layout/default";
+import "@components/ButtonArrow"
+import "@components/ButtonSetting"
+import "@components/SettingsFormInput"
+import "@components/LinkCommon"
+
+
