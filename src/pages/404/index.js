d477912c330a811e54430ac4614d6abc6ac109fc
eprikhodko <eprikhodkodev@gmail.com>
2023-11-26 23:53:23 +0100

    clean up error pages imports

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -4,9 +4,6 @@ import "@src/main.css";
 import "./404.css";
 // layouts, components, modules
 import "@components/layout/default";
-import "@components/ButtonArrow"
-import "@components/ButtonSetting"
-import "@components/SettingsFormInput"
 import "@components/LinkCommon"
 
 
--- src/pages/500/index.js
+++ src/pages/500/index.js
@@ -4,9 +4,6 @@ import "@src/main.css";
 import "./500.css";
 // layouts, components, modules
 import "@components/layout/default";
-import "@components/ButtonArrow"
-import "@components/ButtonSetting"
-import "@components/SettingsFormInput"
 import "@components/LinkCommon"
 
 
