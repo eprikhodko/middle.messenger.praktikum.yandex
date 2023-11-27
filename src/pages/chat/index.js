c511a035bb2211aafc0fc68eff39f3c69277afae
eprikhodko <eprikhodkodev@gmail.com>
2023-11-27 01:30:28 +0100

    fix build error

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,7 +1,5 @@
 // global style
 import "@src/main.css";
-// page style
-import "./chat.css";
 // layouts, components, modules
 import "@components/layout/default";
 import "@components/layout/formCommonLayout";
