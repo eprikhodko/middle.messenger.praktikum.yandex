cae3564c06b1ba965bca9113196a384284205bfc
eprikhodko <eprikhodkodev@gmail.com>
2023-12-06 19:11:41 +0100

    add initial handlebars precompile setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import "./main.css";
+import "./index.css";
+
+// import {HomePage} from './pages/Home';
+// import {Button} from './components/Button';
+// import {registerComponent} from "./utils/resgiterComponent";
+// import Card from "./components/Card";
+// import {render} from "./utils/render";
+
+// registerComponent('Button', Button);
+// registerComponent('Card', Card);
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   render('home')
+// });
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import handlebars from "vite-plugin-handlebars";
-import Handlebars from "handlebars";
+// import handlebars from "vite-plugin-handlebars";
+// import Handlebars from "handlebars";
+import vitePluginHandlebarsPrecompile from "./vite-plugin-handelbars-precompile";
 import postcssNesting from "postcss-nesting";
 
 const root = resolve(__dirname, "src");
@@ -22,7 +23,7 @@ export default defineConfig({
         userProfile: resolve(root, "pages/profile/profile.html"),
         changePassword: resolve(
           root,
-          "pages/change-password/change-password.html",
+          "pages/change-password/change-password.html"
         ),
         notFoundPage: resolve(root, "pages/404/404.html"),
         serverErrorPage: resolve(root, "pages/500/500.html"),
@@ -37,28 +38,29 @@ export default defineConfig({
     },
   },
   plugins: [
-    handlebars({
-      partialDirectory: [resolve(root, "components"), resolve(root, "modules")],
-      context: {
-        username: "John",
-      },
-      helpers: {
-        LinkCommon: (text, options) => {
-          const attributes = [];
+    vitePluginHandlebarsPrecompile(),
+    // handlebars({
+    //   partialDirectory: [resolve(root, "components"), resolve(root, "modules")],
+    //   context: {
+    //     username: "John",
+    //   },
+    //   helpers: {
+    //     LinkCommon: (text, options) => {
+    //       const attributes = [];
 
-          Object.keys(options.hash).forEach((key) => {
-            const escapedKey = Handlebars.escapeExpression(key);
-            const escapedValue = Handlebars.escapeExpression(options.hash[key]);
-            attributes.push(escapedKey + '="' + escapedValue + '"');
-          });
-          const escapedText = Handlebars.escapeExpression(text);
+    //       Object.keys(options.hash).forEach((key) => {
+    //         const escapedKey = Handlebars.escapeExpression(key);
+    //         const escapedValue = Handlebars.escapeExpression(options.hash[key]);
+    //         attributes.push(escapedKey + '="' + escapedValue + '"');
+    //       });
+    //       const escapedText = Handlebars.escapeExpression(text);
 
-          const escapedOutput =
-            "<a " + attributes.join(" ") + ">" + escapedText + "</a>";
-          return new Handlebars.SafeString(escapedOutput);
-        },
-      },
-    }),
+    //       const escapedOutput =
+    //         "<a " + attributes.join(" ") + ">" + escapedText + "</a>";
+    //       return new Handlebars.SafeString(escapedOutput);
+    //     },
+    //   },
+    // }),
   ],
   css: {
     postcss: {
