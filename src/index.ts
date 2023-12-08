07da691f390017a7f553106d786674d8fe9843b0
eprikhodko <eprikhodkodev@gmail.com>
2023-12-08 17:07:41 +0100

    create initial Block class

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import "./main.css";
 import "./index.css";
 
-// import {HomePage} from './pages/Home';
-// import {Button} from './components/Button';
-// import {registerComponent} from "./utils/resgiterComponent";
+import {HomePage} from './pages/home';
+import {Button} from './components/Button';
+import {registerComponent} from "./utils/registerComponent";
 // import Card from "./components/Card";
-// import {render} from "./utils/render";
+import {render} from "./utils/render";
 
-// registerComponent('Button', Button);
+registerComponent('Button', Button);
 // registerComponent('Card', Card);
 
-// window.addEventListener('DOMContentLoaded', () => {
-//   render('home')
-// });
+window.addEventListener('DOMContentLoaded', () => {
+  render('home')
+});
+
---
+++ src/pages/home/index.ts
@@ -0,0 +1,46 @@
+import Block from '../../utils/Block';
+import template from "./home.hbs";
+import {render} from "../../utils/render";
+
+
+export class HomePage extends Block {
+  constructor() {
+    super({
+      type: 'btn',
+      label: 'root label',
+      onClick: () => {
+        console.log("hello from root button")
+      },
+
+      customButton: {
+        label: 'CUSTOM BUTTON',
+        type: 'submit',
+        onClick: () => {
+            console.log("hello from custom button")
+          }
+        }, 
+
+      buttons: [
+        {
+          label: 'Login', 
+          type: 'sub',
+          onClick: () => {
+            render('login');
+            console.log("render login page")
+          }
+        },
+        {
+          label: 'Button 2',
+          type: 'some-type'
+        },
+        {
+          label: 'Button 3',
+        },
+      ]
+    });
+  }
+
+  render() {
+    return this.compile(template, this.props);
+  }
+}
