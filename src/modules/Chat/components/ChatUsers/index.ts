3b28d02e3f2f0c9e6db61a2d44dd1ebe62f89767
eprikhodko <eprikhodkodev@gmail.com>
2024-01-05 21:33:22 +0100

    update chat users props types

--- src/modules/Chat/components/ChatUsers/index.ts
+++ src/modules/Chat/components/ChatUsers/index.ts
@@ -10,6 +10,11 @@ interface Props {
   events?: {
     click: () => void;
   };
+  chatUsers: [];
+}
+
+export interface IChatUsers {
+  chatUsers: [Record<string, string | number>];
 }
 
 export class ChatUsersBase extends Block<Props> {
@@ -26,10 +31,14 @@ export class ChatUsersBase extends Block<Props> {
     this.children.chatUsersPlaceholder = new ChatUsersPlaceholder({});
   }
 
-  protected componentDidUpdate(oldProps, newProps): boolean {
-    this.children.chatUsersPlaceholder?.setProps({
-      users: newProps.chatUsers.filter((user) => user.role !== "admin"),
-    });
+  protected componentDidUpdate(_oldProps: {}, newProps: Props): boolean {
+    const chatUsersProps = newProps as unknown as IChatUsers;
+    const chatUsersPlaceholderBlock = this.children.chatUsersPlaceholder;
+    if (!Array.isArray(chatUsersPlaceholderBlock)) {
+      chatUsersPlaceholderBlock.setProps({
+        users: chatUsersProps.chatUsers.filter((user) => user.role !== "admin"),
+      });
+    }
 
     return false;
   }
