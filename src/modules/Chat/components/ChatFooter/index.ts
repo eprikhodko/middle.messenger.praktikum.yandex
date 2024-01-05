5490e093350f18a3f1a5c22c6d759a7a4a89bbc0
eprikhodko <eprikhodkodev@gmail.com>
2024-01-05 15:10:57 +0100

    send chat message by pressing enter key

--- src/modules/Chat/components/ChatFooter/index.ts
+++ src/modules/Chat/components/ChatFooter/index.ts
@@ -18,21 +18,46 @@ interface MessengerProps {
   userId: number;
 }
 
-const sendMessageInputProps = {
-  type: InputType.TEXT,
-  name: InputName.MESSAGE,
-  id: "message",
-  placeholder: "Message",
-  pattern: ValidationPattern.MESSAGE,
-  inputClassName: "send-message-form__input",
-};
+// const sendMessageInputProps = {
+//   type: InputType.TEXT,
+//   name: InputName.MESSAGE,
+//   id: "message",
+//   placeholder: "Message",
+//   pattern: ValidationPattern.MESSAGE,
+//   inputClassName: "send-message-form__input",
+//   onKeypress: (event) => {
+//     if (event.key === "Enter") {
+//       // Code to execute when Enter is pressed
+//       console.log("Enter key pressed!");
+//       this.sendMessage()
+//     }
+//   },
+// };
+
+const getSendMessageInputProps = (context) => {
+  return {
+    type: InputType.TEXT,
+    name: InputName.MESSAGE,
+    id: "message",
+    placeholder: "Message",
+    pattern: ValidationPattern.MESSAGE,
+    inputClassName: "send-message-form__input",
+    onKeypress: (event) => {
+      if (event.key === "Enter") {
+        console.log("Enter key pressed!");
+        context.sendMessage()
+      }
+    },
+  }
+}
 
 class ChatFooterBase extends Block<MessengerProps> {
   constructor(props: MessengerProps) {
     super(props);
   }
+
   protected init() {
-    this.children.sendMessageInput = new FormInput(sendMessageInputProps);
+    this.children.sendMessageInput = new FormInput(getSendMessageInputProps(this));
     this.children.buttonSendMessage = new ButtonArrow({
       type: "button",
       onClick: () => {
@@ -46,6 +71,17 @@ class ChatFooterBase extends Block<MessengerProps> {
         }
       },
     });
+  }
+
+  sendMessage() {
+    const input = this.children.sendMessageInput as FormInput;
+    const message = input.getValue();
+
+    input.setValue("");
+
+    if (message) {
+      MessagesController.sendMessage(this.props.selectedChat!, message);
+    }
   }
 
   protected render(): DocumentFragment {
